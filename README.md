e747b1d991b2c70594fcf0b626b923218db81da3
Joseph Perez <sephjo5991@gmail.com>
2024-08-19 17:09:50 -0400

    got betting functions working, now need to start the machine spin process

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # slotproject
 java-script slot machine
+
+Step 1: Deposit some money
+Step 2: Determine number of linesto bet on
+Step 3: Collect a bet amount
+Step 4: Spin the slot machine
+Step 5: Check if the user won
+Step 6: give the user their winnings
+Step 7: play again
+
+
+
---
+++ javascript.js
@@ -0,0 +1,55 @@
+//Deposit some money
+//Determine number of linesto bet on
+//Collect a bet amount
+//Spin the slot machine
+//Check if the user won
+//give the user their winnings
+//play again
+
+const prompt = require("prompt-sync")();
+
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount,try again.");
+        } else {
+            return depositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter number of lines to bet on 1-3: ")
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines,try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+        } else {
+            return numberBet;
+        }
+    }
+};
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
+
+
+
